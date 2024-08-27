a65c7f1598136e63dc747276cf01be9343ce42d1
laurhakob <laurhakob@gmail.com>
2024-08-27 15:37:54 +0400

    already have auth clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 import { Metadata } from "next";
+import {ClerkProvider} from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -19,6 +21,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { 
+          colorPrimary: "#3371FF",
+          fontSize: '16px'
+        },
+      }}
+    >
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
@@ -29,5 +40,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
